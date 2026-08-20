b23c7fac028fb40938ecaea9f87edbc8296db90f
mnahidislam39 <devnahidislam4@gmail.com>
2026-08-21 01:33:24 +0600

    history added

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,8 @@ const routes = [
 ];
 
 const router = createRouter({
-   history: createWebHistory(),
+   // এখানে import.meta.env.BASE_URL যোগ করতে হবে
+   history: createWebHistory(import.meta.env.BASE_URL),
    routes,
    scrollBehavior() {
       return { top: 0 }; 
